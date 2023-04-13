4332d1c3bf2943193c0ed2cb7dfb5938aa8954ce
VasiliyChernyaev <126881468+VasiliyChernyaev@users.noreply.github.com>
2023-04-14 00:05:01 +1000

    ver 2
    исправлено

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-import psycopg2, vk_api
 from tokens import vktoken, group_token
 from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api import VkApi
+from psycopg2 import connect
+from database import delete_tables, create_table, add_user, check_database
 
 # ##Группы для поиска
 # 10360575    87750784 201852825 199864025   202669543 207228548
 # соционика1 соционика2   mbti    mbtimems1  mbtimems2   intp
 
-
-vk_session = vk_api.VkApi(token=group_token)
-user_session = vk_api.VkApi(token=vktoken)
+vk_session = VkApi(token=group_token)
+user_session = VkApi(token=vktoken)
 session_api = vk_session.get_api()
 longpool = VkLongPoll(vk_session)
 
@@ -22,8 +23,6 @@ def get_user_info(id):
                             {'user_id': id,
                             'fields': 'city, sex'})
     return info
-
-
 
 ## Запрос всех пользователей
 def get_user_json(age, gender, city_id, group_id=None, offset=0):
@@ -56,30 +55,13 @@ def get_user_json(age, gender, city_id, group_id=None, offset=0):
                                        )
     return database
 
-def delete_tables(conn): ##Удаление таблицы
-    with conn.cursor() as cur:
-        cur.execute("""Drop table if exists Users;""")
-    return "Таблицы успешно удалены"
-    conn.commit()
-
-def create_db(conn):  ##Функция, создающая структуру БД. Т.е. в данной функции создаются таблицы в базе данных
-    with conn.cursor() as cur:
-        cur.execute("""create table if not exists Users (id serial primary key,
-                    user_id integer not null,
-                    pair_id integer not null,
-                    CONSTRAINT user_pairs UNIQUE(user_id,pair_id)
-                    );""")
-        conn.commit()
-    return 'База данных успешно создана'
-
-def get_users(conn):
+## Список подходящих партнеров
+def get_users(user_id):
     user_list = []
-    try:
-      for age in range (from_age, to_age + 1):
-          data = get_user_json(age, gender, city_id, group_id, offset=0)
-          count = data['count']
-          for i in range(0, count, 1000):
-            print(i)
+    for age in range(from_age, to_age + 1): ## Идем по возрасту, начиная с молодых
+        data = get_user_json(age, gender, city_id, group_id, offset=0)
+        count = data['count']
+        for i in range(0, count + 1, 1000): ## Работа с оффсетом, если count > 1000
             data = get_user_json(age, gender, city_id, group_id, offset=i)
             for user in data['items']:
                 if user['is_closed'] is False: ## проверка на закрытый профиль
@@ -87,24 +69,14 @@ def get_users(conn):
                         pair_id = user['id']
                         first_name = user['first_name']
                         last_name = user['last_name']
-                        with conn.cursor() as cur:
-                            cur.execute("""Select pair_id from Users u
-                                        WHERE u.user_id =%s and u.pair_id =%s;""", (id, pair_id))
-                            check = cur.fetchone()
-                        if check is None:
+                        if check_database(conn, user_id=user_id, pair_id=pair_id) is None:
                             user_list.append([pair_id, last_name, first_name, age])
-                        else:
-                            continue
-                    else:
-                        continue
-                else:
-                    continue
-    except:
-        pass
+        if len(user_list) > profile_count:  ## Останавливаем цикл for age, если слишком много пользователей
+            break
     return user_list
 
-
-def get_photo_json(id): ##Запрос фотографий
+## Запрос фотографий
+def get_photo_json(id):
     photos = user_session.method('photos.get', {
                                 'owner_id': id,
                                 'album_id': 'profile',
@@ -113,75 +85,96 @@ def get_photo_json(id): ##Запрос фотографий
                                 })
     return photos
 
+## Фотографии пользователей
+def get_photos(id):
+    fotos = {}
+    data = get_photo_json(id=id)
+    if 'error' not in data:  ##error при достижении лимита запросов
+        items = data['items']
+        for item in items:
+            for photos in item['sizes']:  ##выбираем самые большие фотки и фильтруем по лайкам+комментариям
+                sizes = []
+                photo_size = photos['width'] + photos['height']
+                photo_rating = item['likes']['count'] + item['comments']['count']
+                sizes.append([photo_size, photo_rating, photos['url'].split('&c_uniq_tag')[0]])
+                max_size = max(sizes)
+            fotos.update({max_size[2]: max_size[1]})
+            top3photos = sorted(fotos, key=fotos.get, reverse=True)[:3]
+    return top3photos
 
-with psycopg2.connect(database="vkinder", user="postgres", password="postgres") as conn:
-    # delete_tables(conn)
-    create_db(conn)
-    get_users(conn)
-
+## Переменные для дополнительных параметров поиска (по умолчанию)
+first_message = 0
+anketi = 0
+groups = 0
+profile_count = 100 ## Сколько анкет по умолчанию
 ## Держурство бота
 for event in longpool.listen():
     if event.type == VkEventType.MESSAGE_NEW:
         if event.to_me:
             msg = event.text.lower()
             id = event.user_id
-            greetings = ["hi", "привет", "здравствуйте", "123", "хай", "hello", "как дела?"]
-            if msg in greetings:
+            ## Первое сообщение боту
+            if first_message == 0:
                 send_some_message(id, f'Здравствуйте пользователь {id}, Вас приветствует ассистент Vkinder. '
                                       f'Чтобы начать поиск, введите "поиск".')
+                first_message = 1
+            ## Берем данные пользователя
             elif msg == 'поиск':
-                city_id = get_user_info(id)[0]["city"]["id"]
+                city_id = get_user_info(id=id)[0]["city"]["id"]
+                group_id = None
                 send_some_message(id, f'Мы ищем пару из {get_user_info(id=id)[0]["city"]["title"]}, '
                                       f'введите желаемый возраст через диапазон, например "18, 55"')
                 if get_user_info(id=id)[0]["sex"] == 1:
                     gender = 2
                 elif get_user_info(id=id)[0]["sex"] == 2:
                     gender = 1
+            ## Возрастной диапазон, через запятую from_age, to_age
             elif len(msg.split(',')) == 2:
                 try:
                     from_age = int(msg.split(',')[0])
                     to_age = int(msg.split(',')[-1])
-                    group_id = None
-                    send_some_message(id, 'Нужны дополнительные параметры (группы)? '
-                                          'Если хотите, введите "группы". Иначе введите "начать поиск"')
+                    send_some_message(id, 'Нужны дополнительные параметры (группы, кол-во анкет)? '
+                                          'Если хотите, введите "группы" или "анкеты". Иначе введите "начать поиск"')
                 except:
-                    print('попробуйте еще раз')
+                    send_some_message(id, 'Ошибка диапазона, если вам нужен определенный возраст'
+                                                 'используйте его через запятую, например "25,25"')
+            ## Дополнительная сортировка по одной группе
             elif msg == "группы":
                 send_some_message(id, 'Введите id группы (9 цифр). Узнать id можно на https://regvk.com/id/')
-            elif len(str(msg)) == 9:
+                groups = 1
+            elif groups == 1:
                 try:
-                    if type(int(msg)) == int:
+                    if len(str(msg)) == 9 and type(int(msg)) == int:
                         group_id = int(msg)
-                        send_some_message(id, 'Пора начинать! Ведите "начать поиск"')
+                        send_some_message(id, 'Сортировка по группе добавлена! Ведите "начать поиск"')
+                        groups = 0
                 except:
-                    pass
-
+                    send_some_message(id, "ID группы состоит только из цифр")
+            ## Даем пользователю ввести сколько ему нужно анкет
+            elif msg == "анкеты":
+                send_some_message(id, 'Введите нужное количество анкет, максимум 1000 (по умолчанию 100)')
+                anketi = 1
+            elif anketi == 1:
+                try:
+                    if int(msg) in range(1, 1001):
+                        profile_count = int(msg)
+                        send_some_message(id, f'Количество отображаемых анкет — {profile_count}. Можно начинать поиск.')
+                        anketi = 0
+                    else:
+                        send_some_message(id, 'Используйте диапазон от 1 до 1000.')
+                except:
+                    send_some_message(id, "Введите число без каких-либо других символов")
+            ## Начинаем поиск, подключаемся к БД
             elif msg == "начать поиск":
-                for user in get_users(conn):
-                    send_some_message(id, f'{"https://vk.com/id" + str(user[0])} {user[1]}, {user[2]}, {user[3]}')
-                    data = get_photo_json(id=user[0])
-                    fotos = {}
-                    if 'error' not in data:  ##error при достижении лимита запросов
-                        items = data['items']
-                        for item in items:
-                            for photos in item['sizes']:  ##выбираем самые большие фотки и фильтруем по лайкам+комментариям
-                                sizes = []
-                                x = photos['width'] + photos['height']
-                                y = item['likes']['count'] + item['comments']['count']
-                                sizes.append([x, y, photos['url'].split('&c_uniq_tag')[0]])
-                                max_size = max(sizes)
-                            fotos.update({max_size[2]: max_size[1]})
-                            s = sorted(fotos, key=fotos.get, reverse=True)[:3]
-                    if len(s) == 1:
-                        send_some_message(id, f'{s[0]}\n')
-                    if len(s) == 2:
-                        send_some_message(id, f'{s[0]}\n{s[1]}\n')
-                    if len(s) == 3:
-                        send_some_message(id, f'{s[0]}\n{s[1]}\n{s[2]}\n')
-                    with conn.cursor() as cur:
-                        cur.execute("""Insert into Users (user_id, pair_id)
-                                    values (%s,%s);""", (id, user[0]))
-                        conn.commit()
-                send_some_message(id, "Поиск завершен. Удачи!")
-conn.close()
-
+                with connect(database="vkinder", user="postgres", password="waterpillar") as conn:
+                    # delete_tables(conn) ## Удалить таблицу, если необходимо
+                    create_table(conn) ## Создаем таблицу user + пара, когда пользователь запустил поиск
+                    try:
+                        for user in get_users(user_id=id)[:profile_count]: ## кол-во профилей за сессию
+                            send_some_message(id, f'{"https://vk.com/id" + str(user[0])} {user[1]}, {user[2]}, {user[3]}')
+                            send_some_message(id, '\n'.join(str(link) for link in get_photos(id=user[0])))
+                            add_user(conn, user_id=id, pair_id=user[0]) ##Добавили пару в БД
+                    except:
+                        send_some_message(id, "Ошибка запроса, попробуте позже")
+                    send_some_message(id, "Поиск завершен. Удачи!")
+                conn.close()
